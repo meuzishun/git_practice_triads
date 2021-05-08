719272c6cdaf8ff278763fb46b37e772571688c7
Andrew Smith <meuzishun@gmail.com>
2021-05-08 17:00:34 -0400

    Add audio file loading functions

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+const audioCtx = new AudioContext();
+
+async function getFile(audioContext, filepath) {
+    const response = await fetch(filepath);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+}
+
+async function setupSample(audioFile) {
+    const sample = await getFile(audioCtx, audioFile);
+    return sample;
+}
+
 const triads = [...document.querySelectorAll('.triad')];
 
 const reportInfo = function(evt) {
