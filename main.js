fb05408b53c457718ecc48a03552cefa036631aa
Andrew Smith <meuzishun@gmail.com>
2021-05-08 16:37:46 -0400

    Add function to get audio file name

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const triads = [...document.querySelectorAll('.triad')];
 
 const reportInfo = function(evt) {
-    const elem = evt.target;
-    const parent = elem.parentElement;
-    console.log(parent);
-    console.log(elem);
+    const fileName = evt.target.dataset.audioFile;
+    console.log(fileName);
 }
 
 triads.forEach(triad => triad.addEventListener('click', reportInfo));
